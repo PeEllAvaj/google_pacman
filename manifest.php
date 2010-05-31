10b9e9a0c157fefb60439716e71792d70ed6cdd8
Stephen Fluin <stephen@mistuph.com>
2010-05-31 03:13:23 -0500

    Tweaked for offline android use

---
+++ manifest.php
@@ -0,0 +1,16 @@
+<?php
+
+header("Content-Type: text/cache-manifest");
+?>CACHE MANIFEST
+# v1
+CACHE:
+/pacman/index.html
+/pacman/src/pacman10-hp.gif
+/pacman/src/pacman10-hp.png
+/pacman/src/pacman10-hp-sprite-2.png
+/pacman/src/pacman10-hp-sprite.png
+/pacman/src/swf/pacman10-hp-sound.swf
+/pacman/src/js/pacman10-hp.2.js
+/pacman/src/js/pacman10-hp.3.js
+/pacman/src/js/pacman10-hp.js
+
